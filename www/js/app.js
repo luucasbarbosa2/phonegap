64f0060fa226ee22b95a91bc6c9f3fcd7baa38c9
lucasbarbosa <lucasbarbosa@IFSPSAOCARLOS.EDU.BR>
2018-10-29 18:25:18 -0300

    second2222

--- www/js/app.js
+++ www/js/app.js
@@ -1,4 +1,25 @@
-document.addEventListener("deviceready", onDeviceReady, false);
-function onDeviceReady() {
-$("#app").html("Hello, World! Demo JQuery Mobile com PhoneGap 2!");
-}
+$(document).on('deviceready', function() {
+
+    $.ajax({
+        url: 'http://18.206.179.76/neo4j/phonegap',
+        type: 'POST',
+        dataType: 'json',
+        data: {
+            param1: 'value1'
+        },
+    }).done(function() {
+        console.log("success");
+    }).fail(function() {
+        console.log("error");
+    }).always(function(data) {
+        console.log(data);
+        console.log("complete");
+    });
+
+    navigator.vibrate(3000);
+
+    $(".listening").click(function(event) {
+        navigator.vibrate(3000);
+    });
+    
+});
